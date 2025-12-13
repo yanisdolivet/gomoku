04b7a4b83d502e5ba80a925f0a6161f5712b64a6
Enzo Gaggiotti <enzo.gaggiotti@epitech.eu>
2025-12-13 14:18:14 +0100

    [UPDATE] Main to implement Parser in logic

--- src/Main.cpp
+++ src/Main.cpp
@@ -15,15 +15,22 @@
  * and std::cin.tie(NULL) for better performance during gameplay.
  */
 int main() {
-  Logger logger;
+  std::srand(static_cast<unsigned int>(std::time(nullptr)));
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(NULL);
 
-  logger.initLogger();
-  logger.addLog("Gomoku game started");
-  Parser parser(logger);
-  parser.runParser();
-  logger.addLog("Gomoku game ended");
-  logger.closeLogFile();
-  return 0;
+  Logger::getInstance().initLogger();
+  Logger::getInstance().addLog("Gomoku game started");
+
+    try {
+        Parser parser;
+        parser.runParser();
+    } catch (const std::exception& e) {
+        Logger::getInstance().addLog("CRITICAL ERROR: " + std::string(e.what()));
+        Logger::getInstance().closeLogFile();
+        return 1;
+    }
+    Logger::getInstance().addLog("Gomoku game ended");
+    Logger::getInstance().closeLogFile();
+    return 0;
 }
